3d6b53aacdf7f90bb4d6a3a6ebea907038570a89
grvx_git <gaurav.4source@gmail.com>
2023-08-10 18:09:20 +0530

    dss

--- src/app/Components/Portfolio.js
+++ src/app/Components/Portfolio.js
@@ -29,68 +29,68 @@ export const Portfolio = () => {
 		setShow("none")
 	}
 	return (
-		<div className={styles.pWrapper}>
-			<h2 className='formula'>Our Portfolio</h2>
-			<div className="container-fluid">
-				<div className={`row mi-a ${styles.swiperWrapper}`}>
-					<Swiper
-						spaceBetween={10}
-						navigation={true}
-						thumbs={{ swiper: thumbsSwiper }}
-						modules={[FreeMode, Thumbs]}
-						className="mySwiper2">
-						{PortfolioData.map((card, index) => {
-							return (
-								<SwiperSlide key={index}>
-									<div className={`${styles.pCard} col-lg-9 col-sm-12 mi-a`} onMouseOver={handleMouseOver}
-									onMouseOut={handleMouseOut} data-aos="fade-up">
-										<div className="card">
-											<div className="card-img-top">
-												<Image className={styles.cardImg} src={card.img} alt="" width={1000} height={1000} />
-											</div>
-											<div className={`card-body ${styles.cbd}`} style={{ display: show}}>
-												<h4 className="card-title">{card.title}</h4>
-												<p className="card-text">{card.text}</p>
-												<Link className={styles.cardLink} href="#">
-													<p>See More</p>
-												</Link>
-											</div>
-										</div>
-									</div>
-								</SwiperSlide>
-							);
+		// <div className={styles.pWrapper}>
+		// 	<h2 className='formula'>Our Portfolio</h2>
+		// 	<div className="container-fluid">
+		// 		<div className={`row mi-a ${styles.swiperWrapper}`}>
+		// 			<Swiper
+		// 				spaceBetween={10}
+		// 				navigation={true}
+		// 				thumbs={{ swiper: thumbsSwiper }}
+		// 				modules={[FreeMode, Thumbs]}
+		// 				className="mySwiper2">
+		// 				{PortfolioData.map((card, index) => {
+		// 					return (
+		// 						<SwiperSlide key={index}>
+		// 							<div className={`${styles.pCard} col-lg-9 col-sm-12 mi-a`} onMouseOver={handleMouseOver}
+		// 							onMouseOut={handleMouseOut} data-aos="fade-up">
+		// 								<div className="card">
+		// 									<div className="card-img-top">
+		// 										<Image className={styles.cardImg} src={card.img} alt="" width={1000} height={1000} />
+		// 									</div>
+		// 									<div className={`card-body ${styles.cbd}`} style={{ display: show}}>
+		// 										<h4 className="card-title">{card.title}</h4>
+		// 										<p className="card-text">{card.text}</p>
+		// 										<Link className={styles.cardLink} href="#">
+		// 											<p>See More</p>
+		// 										</Link>
+		// 									</div>
+		// 								</div>
+		// 							</div>
+		// 						</SwiperSlide>
+		// 					);
 
-						}
+		// 				}
 
-						)}
-					</Swiper>
-					<div className={`${styles.bottomContainer}`}>
-					<Swiper
-						onSwiper={setThumbsSwiper}
-						spaceBetween={8}
-						slidesPerView={3}
-						freeMode={true}
-						watchSlidesProgress={true}
-						modules={[FreeMode, Navigation, Thumbs]}
-						className="mySwiper"
-					>
-						{PortfolioData.map((card, index) => {
-							return (
+		// 				)}
+		// 			</Swiper>
+		// 			<div className={`${styles.bottomContainer}`}>
+		// 			<Swiper
+		// 				onSwiper={setThumbsSwiper}
+		// 				spaceBetween={8}
+		// 				slidesPerView={3}
+		// 				freeMode={true}
+		// 				watchSlidesProgress={true}
+		// 				modules={[FreeMode, Navigation, Thumbs]}
+		// 				className="mySwiper"
+		// 			>
+		// 				{PortfolioData.map((card, index) => {
+		// 					return (
 						
-								<SwiperSlide key={index}>
-									<Image className={styles.bImg} src={card.img} alt="" width={1000} height={1000} />
-								</SwiperSlide>
+		// 						<SwiperSlide key={index}>
+		// 							<Image className={styles.bImg} src={card.img} alt="" width={1000} height={1000} />
+		// 						</SwiperSlide>
 								
-							);
-						}
+		// 					);
+		// 				}
 
-						)}
-					</Swiper>
-					</div>
-				</div>
-			</div>
-		</div>
-
+		// 				)}
+		// 			</Swiper>
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// </div>
+		<div></div>
 		
 	);
 };
